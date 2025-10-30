e54d6f2242aecc8d1702453571bf3df55de17385
Diogo <hi@luvsick.gg>
2025-10-30 10:21:49 +0000

    add next event

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,18 +1,53 @@
 const points = localStorage.getItem("points");
 
-const updatePoints = () => {
+const updateLocalStorage = () => {
   localStorage.setItem(
     "points",
     JSON.stringify({
-      max: Math.floor(Math.random() * 601),
-      yuki: Math.floor(Math.random() * 100),
+      max: 321,
+      yuki: 28,
+    }),
+  );
+
+  const eventDate = new Date("2025-11-09T17:00:00");
+  localStorage.setItem(
+    "nextEvent",
+    JSON.stringify({
+      name: "São Paulo",
+      date: eventDate.toISOString(),
     }),
   );
 };
 
-updatePoints();
+updateLocalStorage();
 
 const maxPoints = (document.getElementById("max-points").textContent =
   `${JSON.parse(localStorage.getItem("points")).max}`);
 const yukiPoints = (document.getElementById("yuki-points").textContent =
   `${JSON.parse(localStorage.getItem("points")).yuki}`);
+
+const eventName = document.getElementById("event-name");
+const nextEvent = JSON.parse(localStorage.getItem("nextEvent"));
+eventName.textContent = nextEvent.name;
+
+// Countdown Timer
+const countdownElement = document.getElementById("countdown-event");
+
+const updateCountdown = () => {
+  const nextEvent = JSON.parse(localStorage.getItem("nextEvent"));
+  const eventDate = new Date(nextEvent.date);
+  const now = new Date();
+  const difference = eventDate - now;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  countdownElement.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
