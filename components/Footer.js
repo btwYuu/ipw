6ffdc18077a9038593951dd6f5e888e18da563e7
Diogo <hi@luvsick.gg>
2025-10-30 10:21:37 +0000

    move components to another folder

--- components/Footer.js
+++ components/Footer.js
@@ -8,27 +8,13 @@ class Footer extends HTMLElement {
 <style>
   footer {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     margin-top: 50px;
     text-decoration: none;
-  }
-
-  footer .links a {
-    display: inline-block;
-    padding-left: 1rem;
-  }
-
-  footer .links a:hover {
-    cursor: pointer;
-    color: #db0a40;
   }
 </style>
 <footer> 
   <p>&copy; ${new Date().getFullYear()} Red Bull Racing Ltd</p>
-  <div class="links">
-    <span><a href="./sobre.html">Sobre</a></span>
-    <span><a href="./contacto.html">Contacto</a></span>
-  </div>
 </footer>
 `;
   }
